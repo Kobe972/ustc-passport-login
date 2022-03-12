4c544910e5a6b67b1595c39add8381a9d44a7d64
Kobe972 <71507738+Kobe972@users.noreply.github.com>
2022-03-12 18:08:04 +0800

    Update ustclogin.py

--- ustclogin.py
+++ ustclogin.py
@@ -40,18 +40,17 @@ class Login:
             'CAS_LT':CAS_LT,
             'LT':LT
         }
-        post=self.session.post('https://passport.ustc.edu.cn/login', data=data,headers=headers)
-        return post
+        self.result=self.session.post('https://passport.ustc.edu.cn/login', data=data,headers=headers)
         
     def login(self):
         self.session=requests.Session()
         loginsuccess = False
         retrycount = 5
         while (not loginsuccess) and retrycount:
-            result=self.passport()
+            self.passport()
             self.cookies = self.session.cookies
             retrycount = retrycount - 1
-            if result.url=='https://passport.ustc.edu.cn/login':
+            if self.result.url=='https://passport.ustc.edu.cn/login':
                 print("Login Failed! Retry...")
             else:
                 print("Login Successful!")
